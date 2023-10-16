aa8c8b1716152eb66d87de6abeb93428ea8cc082
REGENCY-14 <osmantimtonizakaria14@gmail.com>
2023-10-16 12:26:26 +0000

    Initial commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,19 @@
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="links">
+                <ul>
+                    <li>Courses</li>
+                    <li>About</li>
+                    <li>Financial Aid</li>
+                    <li>Community</li>
+                <button>Kenya</button>
+
+                </ul>
+                </div>
+</nav> 
+      );
+}
+ 
+export default Navbar;
