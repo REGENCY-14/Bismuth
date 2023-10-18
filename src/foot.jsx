a2fa32a0533ebfec913a5d95d58d3f77d2ba1143
REGENCY-14 <osmantimtonizakaria14@gmail.com>
2023-10-18 14:45:14 +0000

    footer

---
+++ src/foot.jsx
@@ -0,0 +1,14 @@
+const Foot = () => {
+    return (
+      <div className="footer">
+        <p className="cc">
+          Courses <br /> Careers <br /> FAQs <br /> Contact Us <br /> Privacy
+          Policy <br /> Events
+        </p>
+
+       
+      </div>
+    );
+}
+ 
+export default Foot;
