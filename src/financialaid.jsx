92c0381365cfb289c1f82003382500c9a564f61f
AndyGrld <101481355+AndyGrld@users.noreply.github.com>
2023-11-20 22:59:49 +0000

    Update financialaid.jsx

--- src/financialaid.jsx
+++ src/financialaid.jsx
@@ -4,6 +4,7 @@ const Financial = () => {
             <div className="ffirstpart">
                 <h2>Bitlabs Financial Aid</h2>
             </div>
+            <h1>fads</h1>
         </>
      );
 }
