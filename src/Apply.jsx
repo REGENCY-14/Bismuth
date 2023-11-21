d01318669db0e732afd99db88760834086d27116
REGENCY-14 <osmantimtonizakaria14@gmail.com>
2023-11-21 02:06:36 +0000

    jotform

--- src/Apply.jsx
+++ src/Apply.jsx
@@ -1,12 +1,18 @@
-const Apply = () => {
-    return ( 
-        <>
-             <div className="formcontainer">
+// import { Helmet } from "react-helmet";
+import JotformEmbed from "react-jotform-embed";
 
-        </div>
-        </>
-       
-     );
-}
- 
+const Apply = () => {
+  return (
+    <>
+      {/* <Helmet>
+        <script
+          type="text/javascript"
+          src="https://form.jotform.com/jsform/233230093591552"
+     ></script>
+      </Helmet> */}
+            <JotformEmbed src="https://form.jotform.com/233230093591552"/>
+    </>
+  );
+};
+
 export default Apply;
--- src/financialaid.jsx
+++ src/financialaid.jsx
@@ -1,12 +1,25 @@
 const Financial = () => {
-    return ( 
-        <>
+    return (
+      <div>
             <div className="ffirstpart">
-                <h2>Bitlabs Financial Aid</h2>
-            </div>
-            <h1>fads</h1>
-        </>
-     );
+                <div className="fcontent">
+          <h1>Bismuth Financial Aid</h1>
+          <p>
+            Bitlabs sponsors students, especially those facing challenges, to
+            participate in STEM projects, demonstrating a commitment to
+            diversity. The sponsorship includes financial aid, mentorship, and
+            resource access, breaking down barriers for students with limited
+            opportunities. This initiative aligns with Bitlabs' goal of creating
+            an inclusive STEM education environment, fostering diversity for a
+            more innovative future workforce. For detailed information,
+            contacting Bitlabs directly or referring to official communications
+            is recommended.
+                    </p>
+                    </div>
+                <div className="image"></div>
+        </div>
+      </div>
+    );
 }
  
 export default Financial;
