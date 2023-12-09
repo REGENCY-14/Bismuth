cf54c9ff4f471dad4423c789bd11d07139a87636
REGENCY-14 <osmantimtonizakaria14@gmail.com>
2023-12-09 17:49:22 +0000

    final

--- src/Apply.jsx
+++ src/Apply.jsx
@@ -10,7 +10,21 @@ const Apply = () => {
           src="https://form.jotform.com/jsform/233230093591552"
      ></script>
       </Helmet> */}
-            {/* <JotformEmbed src="https://form.jotform.com/233230093591552"></JotformEmbed> */}
+      {/* <JotformEmbed src="https://form.jotform.com/233230093591552"></JotformEmbed> */}
+      <div className="form">
+        <iframe
+          className="thy"
+          src="https://docs.google.com/forms/d/e/1FAIpQLSezPc0iXSUCMlJPPWZAefh5DrN3w27_XJWmva1T8UusQNbnTg/viewform?embedded=true"
+          width="640"
+          height="1976"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        >
+          Loading…
+        </iframe>
+      </div>
+    
     </>
   );
 };
